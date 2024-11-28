84b50a3858598acaf638430a6921d68598aa70cc
lirl <lirlu@gmail.com>
2024-11-28 11:04:50 +0800

    upd:

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,9 @@ export default {
     //   resolvers: [],
     // }),
     //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
-    // dts({
-    //   outputDir: 'types'
-    // })
+    dts({
+      outputDir: 'types'
+    })
   ],
   server: {
     proxy: {
